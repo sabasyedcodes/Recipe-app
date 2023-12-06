c301627a2b8a773747b6fb3243adf6e85ba380f5
saba syed <sabaa9018@gmail.com>
2023-12-06 13:30:51 +0530

    printed out food titles

--- foodapp/src/App.jsx
+++ foodapp/src/App.jsx
@@ -2,9 +2,13 @@ import { useState } from "react";
 import Search from "./components/Search";
 
 function App() {
+  const [foodData, setFoodData] = useState([]);
   return (
     <div className="App">
-      <Search />
+      <Search foodData={foodData} setFoodData={setFoodData} />
+      {foodData.map((food) => (
+        <h1>{food.title}</h1>
+      ))}
     </div>
   );
 }
--- foodapp/src/components/Search.jsx
+++ foodapp/src/components/Search.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 const URL = "https://api.spoonacular.com/recipes/complexSearch";
 const API_KEY = "a5af7a58f9794fc8a872bdeb0d6c0dbb";
 
-export default function Search() {
+export default function Search({foodData, setFoodData}) {
   const [query, setQuery] = useState("pizza");
   // syntax of the useEffect hook ()callback func
   // {} dependency array
@@ -11,6 +11,7 @@ export default function Search() {
       const res = await fetch(`${URL}?query=${query}&apiKey=${API_KEY}`);
       const data = await res.json();
       console.log(data.results);
+      setFoodData(data.results);
     }
     fetchFood();
   }, [query]);
