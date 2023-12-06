62a656ad26884a12197c0a74ff2203b661b95abb
saba syed <sabaa9018@gmail.com>
2023-12-06 13:22:52 +0530

    created a call to an api via fetch

--- foodapp/src/components/Search.jsx
+++ foodapp/src/components/Search.jsx
@@ -1,14 +1,18 @@
 import { useEffect, useState } from "react";
+const URL = "https://api.spoonacular.com/recipes/complexSearch";
+const API_KEY = "a5af7a58f9794fc8a872bdeb0d6c0dbb";
 
 export default function Search() {
   const [query, setQuery] = useState("pizza");
   // syntax of the useEffect hook ()callback func
   // {} dependency array
   useEffect(() => {
-    function demo() {
-      console.log("Demo function executed");
+    async function fetchFood() {
+      const res = await fetch(`${URL}?query=${query}&apiKey=${API_KEY}`);
+      const data = await res.json();
+      console.log(data.results);
     }
-    demo();
+    fetchFood();
   }, [query]);
   return (
     <div>
