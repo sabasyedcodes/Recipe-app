e6ac270034e437c71f2aa5d0a2ee29c9f9a7510d
saba syed <sabaa9018@gmail.com>
2023-12-09 14:20:47 +0530

    added container and styled it

--- foodapp/src/App.jsx
+++ foodapp/src/App.jsx
@@ -3,6 +3,8 @@ import Search from "./components/Search";
 import FoodList from "./components/FoodList";
 import Nav from "./components/Nav";
 import "./App.css"
+import Container from "./components/Container";
+import InnerContainer from "./components/InnerContainer";
 
 function App() {
   const [foodData, setFoodData] = useState([]);
@@ -10,7 +12,12 @@ function App() {
     <div className="App">
       <Nav />
       <Search foodData={foodData} setFoodData={setFoodData} />
-      <FoodList foodData={foodData} />
+      {/* nested container */}
+      <Container>
+        <InnerContainer>
+          <FoodList foodData={foodData} />
+        </InnerContainer>
+      </Container>
     </div>
   );
 }
